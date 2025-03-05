cb4d9b6dc6ad7ba7840b2e7d05894f34981264b3
k0rsakov <korsakov.iyu@gmail.com>
2025-03-05 13:32:25 +0300

    Change project research_earthquake: - linter fix

--- src/earthquake_one_day_to_s3.py
+++ src/earthquake_one_day_to_s3.py
@@ -19,7 +19,7 @@ con.sql(
         SELECT
             *
         FROM
-            read_csv_auto('https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2025-01-10&endtime=2025-01-11') AS res    
+            read_csv_auto('https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2025-01-10&endtime=2025-01-11') AS res
     ) TO 's3://prod/raw/earthquake/2025-01-10/2025-02-27_00-00-00.gz.parquet';
     """,
 )
